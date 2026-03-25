de2ad166086a9e64237ae72edee9b2f44a91324c
cassiaemanuelly <cassiaemanuelly05@gmail.com>
2026-03-25 18:20:16 -0300

    feat: adicionada busca por franquia no repositorio

--- src/main/java/com/vaultnerd/api/repository/FilmeRepository.java
+++ src/main/java/com/vaultnerd/api/repository/FilmeRepository.java
@@ -3,5 +3,8 @@ package com.vaultnerd.api.repository;
 import com.vaultnerd.api.filmes.Filme;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FilmeRepository extends JpaRepository<Filme, Long> {
+    List<Filme> findByFranquiaIgnoreCase(String franquia);
 }
